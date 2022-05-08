a85f63387651f268439c4fa337c724df60095380
Picushe <agojeda@outlook.com>
2022-05-08 09:35:31 -0500

    Paso 4:  Requerimiento 2: Las propiedades  y  deberán ser datos de tipo fecha que guarden el momento en que se instancie un nuevo objeto de la clase .

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -3,7 +3,7 @@ const User = require('./../../app/models/User')
 describe("Unit Tests for Users class",() => {
     test('Create an User object ', () => {
       //Aqui invocas el codigo que va usar en tu app
-      const user = new User(1, "carlogilmar", "Carlo", "Bio", "dateCreated", "lastUpdated")
+      const user = new User(1, "carlogilmar", "Carlo", "Bio")
 
       //Aqui validas los resultados de ese codigo
       //Esta es una comparacion que va a igualar el valor de la izquierda con el valor de la derecha (valor esperado)
@@ -11,8 +11,8 @@ describe("Unit Tests for Users class",() => {
       expect(user.username).toBe("carlogilmar")
       expect(user.name).toBe("Carlo")
       expect(user.bio).toBe("Bio")
-      expect(user.dateCreated).toBe("dateCreated")
-      expect(user.lastUpdated).toBe("lastUpdated")
+      expect(user.dateCreated).not.toBeUndefined()//verifica que el valor no se undefined
+      expect(user.lastUpdated).not.toBeUndefined()
     });
     
 })
